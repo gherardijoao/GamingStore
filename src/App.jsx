8311cc9185c9d29a10e89012cedf8d9d165efe63
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-01-10 20:07:47 -0300

    Atualizações no App e nas Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/login/Login";
 import Registrar from "./pages/Registrar/Registrar";
+import AccountScreen from "./pages/accountScreen/AccountScreen";
+import Populares from "./pages/Populares/Populares";
+import Principal from "./pages/Principal/Principal";
+import Recuperar from "./pages/Recuperar/Recuperar";
 
+/* 
+  Componente principal da aplicação.
+  Gerencia rotas e o estado global dos usuários cadastrados e logados.
+*/
 function App() {
   const [usuariosCadastrados, setUsuariosCadastrados] = useState([]);
   const [usuarioLogado, setUsuarioLogado] = useState("");
 
-  // Função para adicionar um novo usuário
+  /* 
+    Função para adicionar um novo usuário ao estado de usuários cadastrados.
+  */
   const adicionarUsuario = (novoUsuario) => {
     setUsuariosCadastrados((prevUsuarios) => [...prevUsuarios, novoUsuario]);
   };
@@ -16,6 +32,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Rota para a página de login. Passa os usuários cadastrados e a função de login como props. */}
         <Route
           path="/login"
           element={
@@ -25,6 +42,7 @@ function App() {
             />
           }
         />
+        {/* Rota para a página de registro. Passa os usuários cadastrados e a função de adição como props. */}
         <Route
           path="/register"
           element={
@@ -34,10 +52,24 @@ function App() {
             />
           }
         />
-
-        {/* Rota padrão redireciona para login */}
+        {/* Rota para a tela de conta do usuário. Passa a lista de contas e o email do usuário logado. */}
+        <Route
+          path="/account"
+          element={
+            <AccountScreen
+              account={usuariosCadastrados}
+              loginEmail={usuarioLogado}
+            />
+          }
+        />
+        {/* Rota para a página de recuperação de senha. */}
+        <Route path="/recover" element={<Recuperar />} />
+        {/* Rota para a página de conteúdos populares. */}
+        <Route path="/populares" element={<Populares />} />
+        {/* Rota para a página principal */}
+        <Route path="/lancamentos" element={<Principal />} />
+        {/* Rota padrão que redireciona para a página de login. */}
         <Route path="/" element={<Navigate to="/login" replace />} />
-        {/* Adicione outras rotas aqui conforme necessário */}
       </Routes>
     </BrowserRouter>
   );
